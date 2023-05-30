4a10d2108f5c42809495913dc49c691d67d0a1c8
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-05-30 16:21:47 +0530

    Added solution - LeetHub

---
+++ Geek-onacci Number - GFG/geekonacci-number.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int geek(int a, int b , int c,int n){
+    if(n==1)
+        return a;
+    if(n==2)
+    return b;
+    if(n==3)
+    return c;
+    return geek(a,b,c,n-1)+geek(a,b,c,n-2)+geek(a,b,c,n-3);
+    
+}
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int  a,b,c,n;
+	    cin>>a>>b>>c>>n;
+	    cout<<geek(a,b,c,n)<<endl;
+	}
+	
+	
+	
+	return 0;
+}
