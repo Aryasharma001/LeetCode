9d7739deebfc48c970c4a4e72f2166e330874a11
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-08-04 19:48:10 +0530

    Time: 3 ms (80.78%), Space: 12.2 MB (19.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -10,16 +10,19 @@ public:
                 break;
             }
         }
+        // if ind==-1 , that means the array is in decreasing order so just reverse the array
         if(ind==-1){
             reverse(nums.begin(),nums.end());
         }
         else{
+            // step 2 find the element just greater than nums[ind] and swap both
             for(int i=n-1;i>ind;i--){
                 if(nums[i]>nums[ind]){
                     swap(nums[i],nums[ind]);
                     break;
                 }
             }
+            // step 3 reverse the remaining array to get the next permutation
             reverse(nums.begin()+ind+1,nums.end());
         }
     }
