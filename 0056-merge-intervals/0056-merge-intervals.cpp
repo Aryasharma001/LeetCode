8b15389df4b21969c06ad5814ff378b6e56aa852
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-08-08 23:47:43 +0530

    Time: 29 ms (92.68%), Space: 19.1 MB (43.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>>merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>v;
-        int s=intervals.size();
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
-    });
-        if(s==1){
-            return intervals;
-        }
-        else{
-            int start=intervals[0][0],end=intervals[0][1];
-            for(int i=1;i<s;i++){
-                if(intervals[i][0]<=end){
-                    end=max(intervals[i][1],end);
-                }
-                else{
-                    v.push_back({start,end});
-                    start=intervals[i][0];
-                    end=intervals[i][1];
-                }
-                }
-                v.push_back({start,end});
-        }
-            return v;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        int min = INT_MAX;
+        int maxi = INT_MIN;
+        vector<vector<int>> ans;
+        
+        for (int i = 0; i < intervals.size(); i++) {
+            if (intervals[i][0] < min)
+                min = intervals[i][0];
+            else if(intervals[i][0]>maxi){
+                ans.push_back({min,maxi});
+                min=intervals[i][0];
+                maxi=intervals[i][1];
+            }
+                
+            
+            if (intervals[i][1] > maxi)
+                maxi = intervals[i][1];
+            
         }
         
-    
+        ans.push_back({min, maxi}); // Add the last interval
+        
+        return ans;
+    }
 };
