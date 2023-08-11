9df626b79891678fc81fdd6e76ba1bf2dab9ea43
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-08-11 22:37:15 +0530

    Time: 3 ms (39.11%), Space: 6.4 MB (6.96%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        // Brute force approach 
+        double ans=1;
+        ans=pow(x,n);
+        return ans;
+            
+    }
+};
