daf0be3527f798cf094300abb1082bd138b4183c
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-08-21 00:06:33 +0530

    Time: 3 ms (97.71%), Space: 8.4 MB (64.92%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        // maxLen will calculate the length of the substring
-        //start is the index from where our substring with no repeated chars is started
-        int maxLen = 0, start = -1;
-
-        for(int i = 0; i < s.size(); i++){
-            // if the element is already visited then mark the index because our new substring with no repeated chars will start from that index
-            // here we did not use dict[s[i]] != -1 because once we update the start to some value let say 5, we can consider the chars that were repeated before 5 so we use dict > start 
-            if(dict[s[i]] > start){
-                start = dict[s[i]];
+        int start = 0, end = s.size() - 1;
+        int ans = 0;
+        
+        vector<int> hash(256, -1); // Initialize hash with -1
+        
+        for (int i = 0; i <= end; i++) {
+            if (hash[s[i]] != -1) {
+                start = max(start, hash[s[i]] + 1);
             }
-            // update the dict array with the position where the char is found
-            dict[s[i]] = i;
-            // Keep calculating the maxLen
-            maxLen = max(maxLen, i - start);
+            
+            ans = max(ans, i - start + 1);
+            hash[s[i]] = i; // Store the index of the character
         }
-        return maxLen;
+        
+        return ans;
     }
 };
+
