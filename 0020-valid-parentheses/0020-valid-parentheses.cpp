afc9fda733d92b0f4e61f84fc8ca3c711cb8a064
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-08-27 13:47:40 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (79.63%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-       int n =s.length();
-    unordered_map<char,char>map = {
-        {'(',')'},  
-        {'[',']'},  
-        {'{','}'}
-    };
-    
-    stack<int>st;
-    
-    for(int i=0;i<n;i++)
-    {
-        if(s[i]=='(' || s[i]=='{' || s[i]=='[')
-            st.push(s[i]);
-        else
-        {
-            if(st.empty() || map[st.top()] != s[i])
-                return false;
-            else
-                st.pop();
+       // code here
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('|| s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()) return false;
+                else{
+                    char c=st.top();
+                    st.pop();
+                    if((c=='('&&s[i]==')')||(c=='['&&s[i]==']')||(c=='{'&&s[i]=='}')){
+                        continue;
+                    }
+                    else
+                    return false;
+                }
+            }
         }
-    }
-    
-    return st.empty();
+        if(st.empty())
+        return true;
+        else
+        return false;
     }
 };
