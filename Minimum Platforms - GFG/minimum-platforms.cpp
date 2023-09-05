1053cd96d02496845ef0c0ea1be4c89955bf5cab
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-09-05 20:11:59 +0530

    Added solution - LeetHub

--- Minimum Platforms - GFG/minimum-platforms.cpp
+++ Minimum Platforms - GFG/minimum-platforms.cpp
@@ -13,25 +13,24 @@ class Solution{
     int findPlatform(int arr[], int dep[], int n)
     {
     	// Your code here
-    	int count=0;
     	sort(arr,arr+n);
     	sort(dep,dep+n);
-    	int ans=1;
-    	int i=0,j=0;
-    	while(i<n){
+    	int i=1,j=0;
+    	int plat_needed=1,result=1;
+    	while(i<n && j<n){
     	    if(arr[i]<=dep[j]){
-    	        count++;
+    	        plat_needed++;
     	        i++;
-    	        ans=max(ans,count);
     	    }
-    	    else{
-    	        count--;
+    	    else if(arr[i]>dep[j]){
+    	        plat_needed--;
     	        j++;
     	    }
-    	    
+    	    if(plat_needed>result)
+    	    result=plat_needed;
     	}
-    	return ans;
     	
+    	return result;
     }
 };
 
