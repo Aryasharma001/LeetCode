52bcab94c1d89d5252c6c4ccf4bd7be9a29c64a3
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-09-16 01:22:38 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -7,17 +7,18 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-
-
 class Solution {
 public:
-    bool f(int i, int sum, int N, int arr[], vector<vector<int>>& dp) {
-        if (sum == 0)
-            return true;
-        if (sum < 0 || i == N)
-            return false;
-        if (dp[i][sum] != -1)
-            return dp[i][sum];
+    bool f(int i, int sum, int N, int arr[],vector<vector<int>>&dp) {
+        
+        
+        if(sum==0)
+        return true;
+        if(sum<0 ||i==N)
+        return false;
+        
+        if(dp[i][sum]!=-1)
+        return dp[i][sum];
 
         // Explore both cases: include the current element or exclude it
         bool include = f(i + 1, sum - arr[i], N, arr, dp);
@@ -28,6 +29,7 @@ public:
 
     int equalPartition(int N, int arr[]) {
         int totalSum = 0;
+        
         for (int i = 0; i < N; i++) {
             totalSum += arr[i];
         }
@@ -36,15 +38,12 @@ public:
         if (totalSum % 2 != 0) {
             return 0;
         }
-
-        // Initialize the dp array with -1 values
-        vector<vector<int>> dp(N, vector<int>(totalSum / 2 + 1, -1));
+        vector<vector<int>>dp(N,vector<int>(totalSum / 2 + 1,-1));
 
         // Check if there's a subset with a sum equal to totalSum/2
         return f(0, totalSum / 2, N, arr, dp) ? 1 : 0;
     }
 };
-
 
 
 
