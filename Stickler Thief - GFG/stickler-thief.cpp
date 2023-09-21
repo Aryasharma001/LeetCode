94d25e5853feb13bc2e2a508bb3dbadf06111958
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-09-22 01:02:03 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.cpp
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -8,21 +8,30 @@ typedef long long int ll;
 class Solution {
 public:
     // Function to find the maximum money the thief can get.
-    int robber(int i,int arr[],vector<int> &dp){
-        if(i<0) return 0;
-        
-        if(dp[i] != -1) return dp[i];
-        
-        int left = arr[i] + robber(i-2,arr,dp);
-        
-        return dp[i] = max(left,robber(i-1,arr,dp));
+    int f(int arr[], int n, int i, vector<int>& dp) {
+        // Base cases
+        if (i >= n) {
+            return 0;
+        }
+        if (dp[i] != -1)
+            return dp[i];
+        // Calculate profit when the current house is robbed
+        int take = f(arr, n, i + 2, dp)+arr[i];
 
+        // Calculate profit when the current house is not robbed
+        int not_take = f(arr, n, i + 1,dp);
+
+        return dp[i] = max(take, not_take);
     }
-    
-    int FindMaxSum(int arr[], int n)
-    {
-        vector<int> dp(n,-1);
-        return robber(n-1,arr,dp);
+
+    int FindMaxSum(int arr[], int n) {
+        // Your code here
+        if (n == 0) {
+            return 0;
+        }
+        int profit = 0;
+        vector<int> dp(n, -1);
+        return f(arr, n, 0,dp);
     }
 };
 
