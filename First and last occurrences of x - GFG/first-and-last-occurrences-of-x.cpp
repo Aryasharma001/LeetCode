3081918cde174052fdf526eccea1ae2459fecdfe
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-09-23 00:25:01 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -6,30 +6,64 @@ using namespace std;
 class Solution
 {
     public:
-    vector<int> find(int arr[], int n , int x )
-    {
-        // code here
-        
-        int a=-1;
-        int b=-1;
-        
-        for(int i=0;i<n;i++){
-            
-            if(arr[i]==x && (a==-1 && b==-1)){
-                a=i;
-                b=i;
-                
-            }
-            else{
-                if(arr[i]==x){
-                    b=i;
-                }
-            }
-            
-            
-        }
-        return {a,b};
+    int predicate(int arr[],int i,int x){
+        if(arr[i]==x)
+        return 1;
+        else
+        return 0;
     }
+    vector<int> find(int arr[], int n, int x) {
+    vector<int> ans;
+    sort(arr, arr + n);
+
+    int start = 0, end = n - 1;
+
+    // Find the first occurrence of x
+    int first_occurrence = -1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == x) {
+            first_occurrence = mid;
+            end = mid - 1;  // Update end to search for the first occurrence to the left
+        } else if (arr[mid] < x) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    // Find the last occurrence of x
+    int last_occurrence = -1;
+
+    start = 0;
+    end = n - 1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == x) {
+            last_occurrence = mid;
+            start = mid + 1;  // Update start to search for the last occurrence to the right
+        } else if (arr[mid] < x) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    if (first_occurrence != -1) {
+        ans.push_back(first_occurrence);
+        ans.push_back(last_occurrence);
+    } else {
+        ans.push_back(-1);
+        ans.push_back(-1);
+    }
+
+    return ans;
+}
+
 };
 
 //{ Driver Code Starts.
