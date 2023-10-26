3c1d4e61e0d11c4d5e5a48fa2006569c9225bda1
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-10-27 00:49:40 +0530

    Time: 32 ms (42.22%), Space: 13.5 MB (47.44%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,21 +1,22 @@
 class Solution {
-public:
-    void dfs(int i, int j, vector<vector<int>>& vis, vector<vector<char>>& grid) {
-        // Check if the cell is out of bounds, already visited, or not part of an island
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || vis[i][j] == 1 || grid[i][j] == '0') {
+    
+  public:
+    void dfs(int i, int j,vector<vector<int>>&vis,vector<vector<char>>&grid){
+         if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || vis[i][j] == 1 || grid[i][j] == '0') {
             return;
         }
-        
-        vis[i][j] = 1; // Mark the cell as visited
-
-        // Recursively visit adjacent cells
+        vis[i][j]=1;
         dfs(i - 1, j, vis, grid); // Up
         dfs(i + 1, j, vis, grid); // Down
         dfs(i, j - 1, vis, grid); // Left
         dfs(i, j + 1, vis, grid); // Right
+                
+            
+        
     }
-
+    // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
+        // Code here
         int cnt = 0;
         int rows = grid.size();
         if (rows == 0) return 0;
@@ -34,4 +35,7 @@ public:
 
         return cnt;
     }
+    
 };
+
+
