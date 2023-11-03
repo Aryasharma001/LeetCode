e426616e3aa53a0c3952b444120f8575f289ce0e
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2023-11-03 13:17:18 +0530

    Time: 0 ms (100.00%), Space: 11.3 MB (54.48%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,12 +1,48 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int index;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)
-                return i;
-            
+        //step one is finding the hinge index
+        if(nums.size()==1){
+            if(nums[0]==target)
+                return 0;
+            else
+                return -1;
         }
+        int index=0;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1]){
+                index=i;
+                break;
+            }
+                
+        }
+        int start=0,end=index;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else
+                end=mid-1;
+        }
+        
+        int left=index+1,right=nums.size()-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else
+                right=mid-1;
+        }
+        
         return -1;
+    
     }
 };
