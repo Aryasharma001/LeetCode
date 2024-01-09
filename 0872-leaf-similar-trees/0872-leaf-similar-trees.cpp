03e14abd67ba29b8a2cc231f9abb09d1e9c2c3fd
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-01-09 23:58:07 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (30.37%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -11,23 +11,32 @@
  */
 
 class Solution {
-    void bottomView(TreeNode* root, vector<int>& arr) {
-        if (root == NULL)
+    void bottomView(TreeNode* root1,vector<int>&arr){
+        if(root1==NULL)
             return;
-
-        if (root->left == NULL && root->right == NULL)
-            arr.push_back(root->val);
-
-        bottomView(root->left, arr);
-        bottomView(root->right, arr);
+        
+        if(root1->left==NULL && root1->right==NULL)
+        arr.push_back(root1->val);
+        else{
+            bottomView(root1->left,arr);
+            bottomView(root1->right,arr);
+            
+        }
     }
-
+    
 public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
-        vector<int> arr1, arr2;
-        bottomView(root1, arr1);
-        bottomView(root2, arr2);
+        vector<int>arr1;
+        vector<int>arr2;
+        bottomView(root1,arr1);
+        bottomView(root2,arr2);
+        
+        return arr1==arr2;
+        
 
-        return arr1 == arr2;
+        
+        
+        
+        
     }
 };
