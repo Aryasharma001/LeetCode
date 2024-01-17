41ea2ebda7151fdcdc513db728b1ddcb36bd422a
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-01-17 20:55:34 +0530

    Time: 3 ms (54.98%), Space: 8.7 MB (20.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         set<int>occurences;
-        unordered_map<int,int>hashMap;
+        map<int,int>hashMap;
         
         for(int i=0;i<arr.size();i++){
             hashMap[arr[i]]++;
