f0c58c1477ff5b438891827089bae92083f4200c
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-01-17 20:53:27 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (9.43%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        set<int>occurences;
+        map<int,int>hashMap;
+        
+        for(int i=0;i<arr.size();i++){
+            hashMap[arr[i]]++;
+        }
+        for(auto it:hashMap){
+            if(occurences.find(it.second)==occurences.end())
+                occurences.insert(it.second);
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
