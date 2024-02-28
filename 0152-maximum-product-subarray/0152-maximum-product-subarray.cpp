28878e9eba5bfbce357e5dba5e2698314acb5e58
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-02-28 22:47:21 +0530

    Time: 363 ms (5.00%), Space: 16.2 MB (31.29%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,24 +2,16 @@ class Solution {
 public:
     
     int maxProduct(vector<int>& nums) {
-        int n=nums.size();
-        int ans=INT_MIN;
-        int prod=1;
-        for(int i=0;i<n;i++){
-            prod*=nums[i];
-            ans=max(ans,prod);
-            if(nums[i]==0)
-                prod=1;
-        }
-        prod=1;
-        for(int i=n-1;i>=0;i--){
-            prod*=nums[i];
-            ans=max(ans,prod);
-            if(nums[i]==0)
-                prod=1;
+        int maxProduct=1;
+        int ans=*max_element(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            maxProduct=nums[i];
+            for(int j=i+1;j<nums.size();j++){
+                maxProduct*=nums[j];
+                ans=max(ans,maxProduct);
+            }
         }
         return ans;
-       // O(N) time complexity and constant space complexity
         
     }
 };
