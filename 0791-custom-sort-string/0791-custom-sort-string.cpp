13c304b63e9687ea2e37ec47e6fd2f08f009018a
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-03-12 00:40:38 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (66.00%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int>hashOrder(27,0);
+        vector<int>hashS(27,0);
+        string ans="";
+        for(int i=0;i<order.size();i++)
+            hashOrder[int(order[i]-'a')]++;
+        for(int i=0;i<s.size();i++)
+            hashS[int(s[i]-'a')]++;
+        for(int i=0;i<order.size();i++){
+            while(hashS[int(order[i]-'a')]-->0){
+                ans+=order[i];
+                
+            }
+                
+        }
+        for(int i=0;i<s.size();i++){
+            if(hashS[int(s[i])-'a']>0)
+                ans+=s[i];
+                hashS[int(s[i]-'a')]--;
+            
+        }
+        return ans;
+    }
+};
