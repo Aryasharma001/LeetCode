cf21cee6a1b87b910348eed69c216c69f4093e58
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-06-01 01:10:00 +0530

    Time: 2420 ms (5.13%), Space: 744.6 MB (5.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    
+   bool bfs(int node,vector<vector<int>>adj,vector<int>&vis,vector<int>&crcvis){
+       vis[node]=1;
+       crcvis[node]=1;
+       for(auto it:adj[node]){
+           if(!vis[it]){
+               if(bfs(it,adj,vis,crcvis)) return true;
+           }
+           else if(vis[it] && crcvis[it]){
+                   return true;
+           }
+       }
+       crcvis[node]=0;
+       return false;
+       
+   }
    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    queue<int> q;
-    int cnt = 0;
-    int V = numCourses; // Use numCourses as the number of vertices
-    vector<int> indegree(V, 0);
-    vector<vector<int>> adj(V);
-
-    for (int i = 0; i < prerequisites.size(); i++) {
-        int u = prerequisites[i][0];
-        int v = prerequisites[i][1];
-        adj[v].push_back(u); // Reversed the direction of the edge
-        indegree[u]++;
-    }
-
-    for (int i = 0; i < V; i++) {
-        if (indegree[i] == 0) {
-            q.push(i);
-        }
-    }
-
-    while (!q.empty()) {
-        int node = q.front();
-        q.pop();
-        cnt++;
-        for (auto it : adj[node]) {
-            indegree[it]--;
-            if (indegree[it] == 0) {
-                q.push(it);
-            }
-        }
-    }
-
-    // If cnt is not equal to V, there is a cycle, and you cannot finish the courses.
-    return cnt == V;
-}
+       int n = numCourses;   
+        vector<vector<int>>adj(n);
+       for(int i=0;i<prerequisites.size();i++){
+           int u=prerequisites[i][0];
+           int v=prerequisites[i][1];
+           adj[u].push_back(v);
+       }
+       vector<int>vis(n,0);
+       vector<int>crcvis(n,0);
+       for(int i=0;i<n;i++){
+           if(!vis[i]){
+               if(bfs(i,adj,vis,crcvis))
+               return false;
+       }
+       }
+       return true;
+       
+   }
 
 };
