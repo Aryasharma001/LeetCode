aa7430a977c8c94e5a8deb25398f9bb6574755c9
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-06-17 22:16:23 +0530

    Time: 3 ms (68.00%), Space: 7 MB (91.95%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        long high;
-        if(c==1)
-            high=c;
-        else
-            high=sqrt(c);
-        
+        long high=sqrt(c);
         long low=0;
         while(low<=high){
             if((low*low)+(high*high)==c)
