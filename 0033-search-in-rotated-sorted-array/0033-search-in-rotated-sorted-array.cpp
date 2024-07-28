8b38955db8a174f8dfe17eb3e0478a208f268f76
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-07-29 03:18:46 +0530

    Time: 2 ms (64.27%), Space: 13.9 MB (21.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,48 +1,33 @@
+
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        //step one is finding the hinge index
-        if(nums.size()==1){
-            if(nums[0]==target)
-                return 0;
-            else
-                return -1;
-        }
-        int index=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]>nums[i+1]){
-                index=i;
-                break;
-            }
-                
-        }
-        int start=0,end=index;
+        int pivot=0;
+        while(pivot<nums.size()-1 && nums[pivot]<nums[pivot+1])
+            pivot++;
+        //cout<<pivot<<endl;
+        int start=0;
+        int end = pivot;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
+            int mid=start+ (end-start)/2;
+            if(nums[mid]==target)
                 return mid;
-            }
-            else if(nums[mid]<target){
+            else if(nums[mid]<target)
                 start=mid+1;
-            }
             else
                 end=mid-1;
         }
-        
-        int left=index+1,right=nums.size()-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]==target){
+        start=pivot+1, end=nums.size()-1;
+        while(start<=end){
+            int mid=start+ (end-start)/2;
+            if(nums[mid]==target)
                 return mid;
-            }
-            else if(nums[mid]<target){
-                left=mid+1;
-            }
+            else if(nums[mid]<target)
+                start=mid+1;
             else
-                right=mid-1;
+                end=mid-1;
         }
-        
         return -1;
-    
     }
 };
