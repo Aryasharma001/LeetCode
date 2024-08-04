9ce3f86e54352718e3b6a7f282874d29872f8c9f
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-08-05 02:27:21 +0530

    Time: 0 ms (100.00%), Space: 15.5 MB (5.72%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,28 +11,19 @@
  */
 class Solution {
 public:
+    void func(TreeNode* root, int level, vector<vector<int>>& res) {
+        if (root == nullptr) return;
+        if (level >= res.size()) {
+            res.push_back(vector<int>()); // Add a new level if it doesn't exist
+        }
+        res[level].push_back(root->val);
+        func(root->left, level + 1, res);
+        func(root->right, level + 1, res);
+    }
+
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        queue<TreeNode*>q;
-        if(!root) return res;
-        q.push(root);
-
-        while(!q.empty()){
-            vector<int>v;
-            int s=q.size();
-            for(int i=0;i<s;i++){
-            TreeNode* temp=q.front(); q.pop();
-            v.push_back(temp->val);
-            if(temp->left)q.push(temp->left);
-            if(temp->right) q.push(temp->right);    
-            }
-            res.push_back(v);
-            
-            
-            }
-
-
-
+        func(root, 0, res);
         return res;
     }
 };
