80b98c6d3d8944a45c5ef2a3079f7f05ea4a4a00
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-08-05 21:24:54 +0530

    Time: 12 ms (9.36%), Space: 17.6 MB (44.49%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -9,25 +9,16 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    void func(TreeNode* root, int level,vector<int>&vis){
-        if(root==NULL)
-           return;
-        if(vis[level]==-1)
-           vis[level]=1;
-         func(root->left,level+1,vis);
-         func(root->right,level+1,vis);  
-    }
-    int maxDepth(TreeNode* root) {
-        vector<int>vis(100000,-1);
-        func(root,0,vis);
-        int ans=0;
-        for(int i=0;i<vis.size();i++){
-            if(vis[i]==1)
-                ans=max(ans,i+1);
-        }
-        return ans;
-        
-    }
+int maxDepth(TreeNode* root) {
+if(root== NULL){
+return 0;
+}
+int left = maxDepth(root->left);
+int right = maxDepth(root->right);
+return max(left,right)+1;
+
+}
 };
