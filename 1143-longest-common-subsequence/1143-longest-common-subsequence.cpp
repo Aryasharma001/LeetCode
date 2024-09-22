f1443b1929e344bce4af63ac8a51f103424f3db1
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-09-23 00:08:31 +0530

    Time: 27 ms (49.47%), Space: 26 MB (60.79%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    int func(string text1,string text2,int i ,int j){
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(text1[i]==text2[j]){
-            return 1+func(text1,text2,i-1,j-1);
-        }
-        else
-            return max(func(text1,text2,i-1,j),func(text1,text2,i,j-1));
-                
-    }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
