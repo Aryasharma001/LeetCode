af359f22f5d1ecef96134140c79e0da8bf63aab2
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-10-12 02:15:28 +0530

    Time: 124 ms (95.56%), Space: 54.8 MB (96.93%) - LeetHub

---
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int smallestChair(vector<vector<int>>& times, int targetFriend) {
+        int targetArrival=times[targetFriend][0];
+        sort(times.begin(),times.end());
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> occupied_chairs;
+        priority_queue<int,vector<int>,greater<int>>available_chairs;
+        int chairs=0;
+        //available_chairs.push(0);
+        for(int i=0;i<times.size();i++){
+            int arrivalTime=times[i][0];
+            int departureTime=times[i][1];
+            int required_chair;
+            while(!occupied_chairs.empty() && occupied_chairs.top().first<=arrivalTime){
+                available_chairs.push(occupied_chairs.top().second);
+                occupied_chairs.pop();
+            }
+            if(available_chairs.empty()){
+                    required_chair=chairs++;
+            }else{
+                required_chair=available_chairs.top();
+                available_chairs.pop();
+            }
+            
+            if(arrivalTime==targetArrival){
+                return required_chair;
+            }
+            occupied_chairs.push({departureTime,required_chair});           
+        }
+        
+        return chairs;
+        
+        
+    }
+};
