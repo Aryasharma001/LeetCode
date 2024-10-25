536a260b812d19ff4a7fa210cef7338b68d3da62
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-10-25 20:21:40 +0530

    Time: 265 ms (29.12%), Space: 89.8 MB (34.62%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -6,20 +6,20 @@ public:
         for(auto it:folder){
             hashSet.insert(it);
         }
-        for(int i=0;i<folder.size();i++){
+        for(auto currFolder:folder){
             bool isSubFolder=false;
-            string s=folder[i];
-            while(!s.empty()){
-                size_t end=s.find_last_of('/');
-                s=folder[i].substr(0,end);
-                if(hashSet.find(s)!=hashSet.end()){
+            string temp=currFolder;
+            while(!currFolder.empty()){
+                size_t end=currFolder.find_last_of('/');
+                currFolder=currFolder.substr(0,end);
+                if(hashSet.find(currFolder)!=hashSet.end()){
                     isSubFolder=true;
                     break;
                 }
                     
             }
             if(isSubFolder==false)
-                ans.push_back(folder[i]);
+                ans.push_back(temp);
             
         }
         return ans;
