4a8b963a60e003a815b73e7c5824154155864ab5
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-11-02 11:35:14 +0530

    Time: 12 ms (97.89%), Space: 33.2 MB (100.00%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    static string makeFancyString(string& s) {
+        int n=s.size(), i=0;
+        char prev='@';
+        for(int len=0; char c: s){
+            if (prev!=c) len=1;
+            else len++;
+            if (len<= 2) 
+                s[i++]=c;
+            prev=c;
+            
+        }
+        s.resize(i);
+        return s;
+         
+    }
+};
