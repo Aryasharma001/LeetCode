98f69d22590a50edb5982b7e144f253737d8c56a
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2024-11-17 01:14:22 +0530

    Time: 0 ms (100.00%), Space: 34.6 MB (9.20%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         vector<int>ans;
-        if(nums.size()==1)
-            return {nums[0]};
-        for(int i=0;i<=nums.size()-k;i++){
-            int temp=nums[i];
-            for(int j=i;j<i+k-1;j++){
-                if(nums[j]>=nums[j+1] || nums[j+1]!=nums[j]+1){
-                    temp=-1;
-                    break;
-                }
-                temp=max(temp,nums[j+1]);
-                    
+        deque<int>dq;
+        for(int i=0;i<nums.size();i++){
+            if(!dq.empty() && dq.front()<=i-k){
+                dq.pop_front();
             }
-            ans.push_back(temp);
+            while(!dq.empty() && nums[dq.back()]!=nums[i]-1){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(i>=k-1){
+                if(dq.size()==k) ans.push_back(nums[dq.back()]);
+                else{
+                    ans.push_back(-1);
+                }
+            }
+            
         }
         return ans;
     }
